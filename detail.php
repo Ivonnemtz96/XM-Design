c7c2c71708bf29504c34ea9347870365ded19a4a
Ivonne Mtz <Ivonnemtz96@outlook.es>
2023-03-03 11:03:02 -0700

    detalles de proyecto ok

---
+++ detail.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+$title = "Detalle de proyecto";
+include('includes/head.php');
+?>
+
+<body>
+    <?
+    include('includes/preloader.php');
+    include('includes/header.php');
+    include('modules/detail.php');
+    include('includes/footer.php');
+    include('includes/scripts.php');
+    ?>
+</body>
+
+</html>
---
+++ modules/detail.php
@@ -0,0 +1,111 @@
+<!-- Start of Breadcrumb section
+	============================================= -->
+<section id="arck-breadcrumb" class="arck-breadcrumb-section-2 position-relative"
+    data-background="/assets/img/bg/ar-shape.png">
+    <div class="slider-side-content position-absolute">
+        <span class="archx-slider-side1 position-absolute"><a
+                href="mailto:ximena@ximenamoncada.com">ximena@ximenamoncada.com</a></span>
+    </div>
+    <div class="container">
+        <div class="arck-breadcrumb-content position-relative text-center headline-2 ul-li">
+            <h1>Detalles de proyecto</h1>
+            <ul>
+                <li><a href="/">Inicio</a></li>
+                <li>Proyecto 1</li>
+            </ul>
+        </div>
+    </div>
+</section>
+<!-- End of Breadcrumb section
+	============================================= -->
+
+<!-- Start of Project Details section
+	============================================= -->
+<section id="arck-project-details" class="arck-project-details-section inner-page-padding">
+    <div class="container">
+        <div class="arck-project-details-img-slider">
+            <div class="arck-project-details-slider">
+                <div class="arck-project-slide-item">
+                    <img src="/assets/img/project/prd1.jpg" alt="">
+                </div>
+                <div class="arck-project-slide-item">
+                    <img src="/assets/img/project/prd1.jpg" alt="">
+                </div>
+                <div class="arck-project-slide-item">
+                    <img src="/assets/img/project/prd1.jpg" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="arck-project-details-text-wrapper">
+            <div class="row">
+                <div class="col-lg-8">
+                    <div class="arck-project-details-text headline pera-content ul-li-block">
+                        <h3>Dunas 103</h3>
+                        <p>
+                            El proyecto Dunas 103 fue creado para ser un espacio atemporal, lleno de encanto, con un
+                            proceso único de selección de muebles a medida. Cuando empezamos a pensar en el mobiliario
+                            de la casa, lo primero que tuvimos en cuenta fueron los caminos. La funcionalidad del
+                            espacio nos permite tomar decisiones estéticas que satisfagan las necesidades de nuestros
+                            clientes.</p>
+
+                        <p>
+                            Prestar atención a la escala fue una parte integral del proceso de diseño. Optamos por
+                            piezas de gran tamaño y peso combinadas con decoraciones de tonos suaves que ayudaron a
+                            equilibrar cada habitación. Algunos de los elementos favoritos de los clientes fueron los
+                            jarrones de arcilla pintados a mano y el candelabro de hierro orgánico que se encuentra en
+                            la parte superior del comedor.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="arck-project-details-feature headline">
+                        <h3>Información de proyecto</h3>
+                        <div class="feature-list ul-li-block">
+                            <ul>
+                                <li>Lugar: <span>San José del Cabo</span></li>
+                                <li>Projecto: <span>Diseño de interiores</span></li>
+                                <li>Diseñada por: <span>XM Design</span></li>
+                            </ul>
+                        </div>
+                        <div class="project-share d-flex justify-content-between">
+                            <span class="title">Síguenos en nuestras redes</span>
+                            <div class="share-social">
+                                <a href="#"><i class="fab fa-facebook-f"></i></a>
+                                <a href="https://twitter.com/xmdesign_co"><i class="fab fa-twitter"></i></a>
+                                <a href="#"><i class="fab fa-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="arck-project-details-img-wrapper popup-gallery">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="arck-project-details-img-item">
+                        <a href="/assets/img/project/pd-1.jpg" title="La elegancia en una imagen">
+                            <img src="/assets/img/project/prd2.jpg" alt="">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="arck-project-details-img-item">
+                        <a href="/assets/img/project/pd-2.jpg" title="Ximena Moncada Design">
+                            <img src="/assets/img/project/prd3.jpg" alt="">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="arck-project-details-img-item">
+                        <a href="/assets/img/project/pd-3.jpg" title="Un espacio más elegante">
+                            <img src="/assets/img/project/prd4.jpg" alt="">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
+<!-- End of Project Details section
+	============================================= -->
