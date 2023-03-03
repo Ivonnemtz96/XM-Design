000600d62a97d99dfe9cf5c87c86e737f63cdc54
Ivonne Mtz <Ivonnemtz96@outlook.es>
2023-03-03 10:28:23 -0700

    galería ok

--- modules/gallery.php
+++ modules/gallery.php
@@ -1,82 +1,93 @@
 <section id="arck-breadcrumb" class="arck-breadcrumb-section-2 position-relative"
-  data-background="/assets/img/bg/ar-shape.png" style="background-image: url(&quot;/assets/img/bg/ar-shape.png&quot;);">
-  <div class="slider-side-content position-absolute">
-    <span class="archx-slider-side1 position-absolute"><a
-        href="mailto:ximena@ximenamoncada.com">ximena@ximenamoncada.com</a></span>
-  </div>
-  <div class="container">
-    <div class="arck-breadcrumb-content position-relative text-center headline-2 ul-li">
-      <h1>Galería</h1>
-      <ul>
-        <li><a href="#">Inicio</a></li>
-        <li>Nuestra galería</li>
-      </ul>
+    data-background="/assets/img/bg/ar-shape.png"
+    style="background-image: url(&quot;/assets/img/bg/ar-shape.png&quot;);">
+    <div class="slider-side-content position-absolute">
+        <span class="archx-slider-side1 position-absolute"><a
+                href="mailto:ximena@ximenamoncada.com">ximena@ximenamoncada.com</a></span>
     </div>
-  </div>
+    <div class="container">
+        <div class="arck-breadcrumb-content position-relative text-center headline-2 ul-li">
+            <h1>Galería</h1>
+            <ul>
+                <li><a href="#">Inicio</a></li>
+                <li>Nuestra galería</li>
+            </ul>
+        </div>
+    </div>
 </section>
 
 <section id="arck-project-feed-2" class="arck-project-feed-section-2 inner-page-padding">
-  <div class="container">
-    <div class="arck-project-feed-content-2">
-      <div class="filtr-container-area grid_2 clearfix">
-        <div class="grid_2-sizer"></div>
-        <div class="grid_2-item grid_2-size-40">
-          <div class="arck-project-feed-item-2">
-            <div class="arck-project-item-2 position-relative">
-              <div class="inner-img">
-                <img src="/assets/img/project/port1.jpg" alt="">
-              </div>
-              
+    <div class="container popup-gallery">
+        <div class="arck-project-feed-content-2">
+            <div class="filtr-container-area grid_2 clearfix">
+                <div class="grid_2-sizer"></div>
+                <div class="grid_2-item grid_2-size-40">
+                    <div class="arck-project-feed-item-2">
+                        <a href="/assets/img/project/p-1.jpg" title="Creando espacios únicos">
+                            <div class="arck-project-item-2 position-relative">
+                                <div class="inner-img">
+                                    <img src="/assets/img/project/port1.jpg" alt="">
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="grid_2-item grid_2-size-60">
+                    <div class="arck-project-feed-item-2">
+                        <a href="/assets/img/project/p-2.jpg" title="Interiores memorables">
+                            <div class="arck-project-item-2 position-relative">
+                                <div class="inner-img">
+                                    <img src="/assets/img/project/port2.jpg" alt="">
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="grid_2-item grid_2-size-60">
+                    <div class="arck-project-feed-item-2">
+                        <a href="/assets/img/project/p-3.jpg" title="Diseñando espacios elegantes">
+                            <div class="arck-project-item-2 position-relative">
+                                <div class="inner-img">
+                                    <img src="/assets/img/project/port3.jpg" alt="">
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="grid_2-item grid_2-size-40">
+                    <div class="arck-project-feed-item-2">
+                        <a href="/assets/img/project/p-6.jpg" title="La elegancia hecha realidad">
+                            <div class="arck-project-item-2 position-relative">
+                                <div class="inner-img">
+                                    <img src="/assets/img/project/port6.jpg" alt="">
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="grid_2-item grid_2-size-60">
+                    <div class="arck-project-feed-item-2">
+                        <a href="/assets/img/project/p-5.jpg" title="Lugares que inspiran">
+                            <div class="arck-project-item-2 position-relative">
+                                <div class="inner-img">
+                                    <img src="/assets/img/project/port5.jpg" alt="">
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="grid_2-item grid_2-size-40">
+                    <div class="arck-project-feed-item-2">
+                        <a href="/assets/img/project/p-4.jpg" title="La perfección de la proporción">
+                            <div class="arck-project-item-2 position-relative">
+                                <div class="inner-img">
+                                    <img src="/assets/img/project/port4.jpg" alt="">
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-        <div class="grid_2-item grid_2-size-60">
-          <div class="arck-project-feed-item-2">
-            <div class="arck-project-item-2 position-relative">
-              <div class="inner-img">
-                <img src="/assets/img/project/port2.jpg" alt="">
-              </div>
-             
-            </div>
-          </div>
-        </div>
-        <div class="grid_2-item grid_2-size-60">
-          <div class="arck-project-feed-item-2">
-            <div class="arck-project-item-2 position-relative">
-              <div class="inner-img">
-                <img src="/assets/img/project/port3.jpg" alt="">
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="grid_2-item grid_2-size-40">
-          <div class="arck-project-feed-item-2">
-            <div class="arck-project-item-2 position-relative">
-              <div class="inner-img">
-                <img src="/assets/img/project/port6.jpg" alt="">
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="grid_2-item grid_2-size-60">
-          <div class="arck-project-feed-item-2">
-            <div class="arck-project-item-2 position-relative">
-              <div class="inner-img">
-                <img src="/assets/img/project/port5.jpg" alt="">
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="grid_2-item grid_2-size-40">
-          <div class="arck-project-feed-item-2">
-            <div class="arck-project-item-2 position-relative">
-              <div class="inner-img">
-                <img src="/assets/img/project/port4.jpg" alt="">
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
-  </div>
 </section>
