e760ede0c1c28ff8bc8978523c0fab6c8945558c
Ivonne Mtz <Ivonnemtz96@outlook.es>
2023-04-12 16:46:37 -0700

    Nueva propuesta en servidor

---
+++ includes/header.php
@@ -0,0 +1,58 @@
+<!-- Progress scroll totop -->
+<div class="progress-wrap cursor-pointer">
+    <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+        <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
+    </svg>
+</div>
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg">
+    <div class="container">
+        <!-- Logo -->
+        <div class="logo-wrapper navbar-brand valign">
+            <a href="index.html">
+                <div class="logo">
+                    <img src="/img/logo.png" class="logo-img" alt="">
+                </div>
+            </a>
+        </div>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span
+                class="icon-bar"><i class="ti-line-double"></i></span> </button>
+        <!-- Navbar links -->
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item dropdown"> <span class="nav-link active"> Home <i class="ti-angle-down"></i></span>
+                    <ul class="dropdown-menu last">
+                        <li class="dropdown-item"><a href="index.html">Home Layout 1</a></li>
+                        <li class="dropdown-item"><a href="index2.html">Home Layout 2</a></li>
+                        <li class="dropdown-item"><a href="index3.html">Home Layout 3</a></li>
+                        <li class="dropdown-item active"><a href="index4.html">Home Layout 4</a></li>
+                        <li class="dropdown-item"><a href="index5.html">Home Layout 5</a></li>
+                        <li class="dropdown-item"><a href="index6.html">Kenburns Slider <em>New</em></a></li>
+                    </ul>
+                </li>
+                <li class="nav-item"><a class="nav-link" href="about.html">About</a></li>
+                <li class="nav-item dropdown"> <span class="nav-link"> Rooms & Suites <i
+                            class="ti-angle-down"></i></span>
+                    <ul class="dropdown-menu last">
+                        <li class="dropdown-item"><a href="rooms.html">Rooms 1</a></li>
+                        <li class="dropdown-item"><a href="rooms2.html">Rooms 2</a></li>
+                        <li class="dropdown-item"><a href="rooms3.html">Rooms 3</a></li>
+                        <li class="dropdown-item"><a href="room-details.html">Room Details</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item"><a class="nav-link" href="restaurant.html">Restaurant</a></li>
+                <li class="nav-item"><a class="nav-link" href="spa-wellness.html">Spa</a></li>
+                <li class="nav-item"><a class="nav-link" href="gallery.html">Gallery</a></li>
+                <li class="nav-item dropdown"> <span class="nav-link"> News <i class="ti-angle-down"></i></span>
+                    <ul class="dropdown-menu last">
+                        <li class="dropdown-item"><a href="news.html">News 1</a></li>
+                        <li class="dropdown-item"><a href="news2.html">News 2</a></li>
+                        <li class="dropdown-item"><a href="post.html">Single Post</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
