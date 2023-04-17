baecc4a204d6fee9f7e45326b2445266f965a2e5
Ivonne Mtz <Ivonnemtz96@outlook.es>
2023-04-17 12:42:06 -0700

    Galería ok

--- modules/gallery.php
+++ modules/gallery.php
@@ -1,11 +1,11 @@
 <!-- Header Banner -->
 <div class="banner-header section-padding valign bg-img bg-fixed" data-overlay-dark="4"
-    data-background="/img/slider/3.jpg">
+    data-background="/img/slider/5.webp">
     <div class="container">
         <div class="row">
             <div class="col-md-12 text-left caption mt-90">
-                <h5>Images & Videos</h5>
-                <h1>Our Gallery</h1>
+                <h5>Transforma tu espacio</h5>
+                <h1>Nuestra Galería</h1>
             </div>
         </div>
     </div>
@@ -15,72 +15,72 @@
     <div class="container-fluid" style="padding: 50px;">
         <div class="row">
             <div class="col-md-12">
-                <div class="section-subtitle">Images</div>
-                <div class="section-title">Image Gallery</div>
+                <div class="section-subtitle">XM Design</div>
+                <div class="section-title">Explora nuevas ideas</div>
             </div>
             <!-- 3 columns -->
             <div class="col-md-4 gallery-item">
-                <a href="/img/slider/7.jpg" title="" class="img-zoom">
+                <a href="/img/slider/8.webp" title="" class="img-zoom">
                     <div class="gallery-box">
-                        <div class="gallery-img"> <img src="/img/slider/7.jpg" class="img-fluid mx-auto d-block"
+                        <div class="gallery-img"> <img src="/img/slider/8.webp" class="img-fluid mx-auto d-block"
                                 alt="work-img"> </div>
                     </div>
                 </a>
             </div>
             <div class="col-md-4 gallery-item">
-                <a href="/img/slider/5.jpg" title="" class="img-zoom">
+                <a href="/img/slider/10.webp" title="" class="img-zoom">
                     <div class="gallery-box">
-                        <div class="gallery-img"> <img src="/img/slider/5.jpg" class="img-fluid mx-auto d-block"
+                        <div class="gallery-img"> <img src="/img/slider/10.webp" class="img-fluid mx-auto d-block"
                                 alt="work-img"> </div>
                     </div>
                 </a>
             </div>
             <div class="col-md-4 gallery-item">
-                <a href="/img/slider/4.jpg" title="" class="img-zoom">
+                <a href="/img/slider/11.webp" title="" class="img-zoom">
                     <div class="gallery-box">
-                        <div class="gallery-img"> <img src="/img/slider/4.jpg" class="img-fluid mx-auto d-block"
+                        <div class="gallery-img"> <img src="/img/slider/11.webp" class="img-fluid mx-auto d-block"
                                 alt="work-img"> </div>
                     </div>
                 </a>
             </div>
             <!-- 2 columns -->
             <div class="col-md-6 gallery-item">
-                <a href="/img/slider/2.jpg" title="" class="img-zoom">
+                <a href="/img/slider/12.webp" title="" class="img-zoom">
                     <div class="gallery-box">
-                        <div class="gallery-img"> <img src="/img/slider/2.jpg" class="img-fluid mx-auto d-block"
+                        <div class="gallery-img"> <img src="/img/slider/12.webp" class="img-fluid mx-auto d-block"
                                 alt="work-img"> </div>
                     </div>
                 </a>
             </div>
             <div class="col-md-6 gallery-item">
-                <a href="/img/slider/1.jpg" title="" class="img-zoom">
+                <a href="/img/slider/1.webp" title="" class="img-zoom">
                     <div class="gallery-box">
-                        <div class="gallery-img"> <img src="/img/slider/1.jpg" class="img-fluid mx-auto d-block"
+                        <div class="gallery-img"> <img src="/img/slider/1.webp" class="img-fluid mx-auto d-block"
                                 alt="work-img"> </div>
                     </div>
                 </a>
             </div>
             <!-- 3 columns -->
             <div class="col-md-4 gallery-item">
-                <a href="/img/rooms/8.jpg" title="" class="img-zoom">
+                <a href="/img/rooms/4.webp" title="" class="img-zoom">
                     <div class="gallery-box">
-                        <div class="gallery-img"> <img src="/img/rooms/8.jpg" class="img-fluid mx-auto d-block"
+                        <div class="gallery-img"> <img src="/img/rooms/4.webp" class="img-fluid mx-auto d-block"
                                 alt="work-img"> </div>
                     </div>
                 </a>
             </div>
             <div class="col-md-4 gallery-item">
-                <a href="/img/rooms/5.jpg" title="" class="img-zoom">
+                <a href="/img/rooms/10.webp" title="" class="img-zoom">
                     <div class="gallery-box">
-                        <div class="gallery-img"> <img src="/img/rooms/5.jpg" class="img-fluid mx-auto d-block"
+                        <div class="gallery-img"> <img src="/img/rooms/10.webp" class="img-fluid mx-auto d-block"
                                 alt="work-img"> </div>
                     </div>
                 </a>
             </div>
             <div class="col-md-4 gallery-item">
-                <a href="/img/rooms/10.jpg" title="" class="img-zoom">
+                <a href="/img/rooms/11.webp" title="" class="img-zoom">
                     <div class="gallery-box">
-                        <div class="gallery-img"> <img src="/img/rooms/10.jpg" class="img-fluid mx-auto d-block"
+                        <div class="gallery-img"> <img src="/img/rooms/11.webp" class="img-fluid mx-auto d-block"
                                 alt="work-img"> </div>
                     </div>
                 </a>
