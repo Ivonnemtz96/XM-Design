4be1850ac7f4fb12ade62dcd434f48ad33126b2e
Ivonne Mtz <Ivonnemtz96@outlook.es>
2023-04-17 14:13:57 -0700

    ok

---
+++ modules/story.php
@@ -0,0 +1,178 @@
+<!-- Header Banner -->
+<div class="banner-header section-padding valign bg-img bg-fixed" data-overlay-dark="4"
+    data-background="/img/slider/2.webp">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 caption mt-90">
+                <!-- <h5></h5> -->
+                <h1>Sobre XM Design</h1>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- About -->
+<section class="about section-padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 mb-30 animate-box" data-animate-effect="fadeInUp">
+                <span>
+                    <i class="star-rating"></i>
+                    <i class="star-rating"></i>
+                    <i class="star-rating"></i>
+                    <i class="star-rating"></i>
+                    <i class="star-rating"></i>
+                </span>
+                <div class="section-subtitle">Exploramos nuevas ideas</div>
+                <div class="section-title">XM Design</div>
+                <p>
+                    Desde el comienzo de su carrera como diseñadora de interiores, Ximena Moncada ha sido conocida por
+                    su compromiso único con la satisfacción del cliente y un servicio de alta calidad. Este estándar de
+                    excelencia le ha permitido hacer crecer su práctica y ampliar su alcance a nivel internacional.
+                </p>
+                <p>
+                    Como emprendedora que acepta desafíos, Ximena comenzó su carrera de Diseño de Interiores en 2001 en
+                    su país natal, Chile. Originalmente llamada DECORAMBIENTES, fundó, desarrolló y dirigió esta exitosa
+                    práctica junto con otras iniciativas empresariales paralelas para ofrecer servicios de diseño de
+                    interiores comerciales y residenciales de clase mundial para algunas de las principales
+                    instituciones financieras de Chile.
+                </p>
+                <p>
+                    Ximena continuó su carrera en los EE.UU. obteniendo un título de Parsons New School of Design en
+                    Nueva York. Actualmente reside en Baja California Sur, una zona del mundo que está experimentando un
+                    crecimiento residencial y comercial explosivo. Se ha asociado con las principales empresas de
+                    desarrollo inmobiliario y mayoristas de muebles para el hogar para llevar su estilo distintivo y
+                    talento profesional al mercado de Baja California.
+                </p>
+                <!-- reservation -->
+                <div class="reservations">
+                    <div class="icon"><span class="flaticon-call"></span></div>
+                    <div class="text">
+                        <p>Llamar</p> <a target="_blank" href="tel:+14243240006">+1 424-324-0006</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col col-md-3 animate-box" data-animate-effect="fadeInUp"> <img src="/img/rooms/21.webp" alt=""
+                    class="mt-90 mb-30"> </div>
+            <div class="col col-md-3 animate-box" data-animate-effect="fadeInUp"> <img src="/img/rooms/2.webp" alt="">
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!-- Reservation & Booking Form -->
+<section class="testimonials">
+    <div class="background bg-img bg-fixed section-padding pb-0" data-background="/img/slider/4.webp"
+        data-overlay-dark="2">
+        <div class="container">
+            <div class="row">
+                <!-- Reservation -->
+                <div class="col-md-5 mb-30 mt-30">
+                    <p><i class="star-rating"></i><i class="star-rating"></i><i class="star-rating"></i><i
+                            class="star-rating"></i><i class="star-rating"></i></p>
+                    <h5>Nuestro equipo de diseñadores
+                        expertos te ayudará a crear un espacio acogedor y elegante que refleje tu estilo y personalidad.
+                    </h5>
+                    <div class="reservations mb-30">
+                        <div class="icon color-1"><span class="flaticon-call"></span></div>
+                        <div class="text">
+                            <p class="color-1">Llamar</p> <a class="color-1" href="tel:+14243240006">+1 424-324-0006</a>
+                        </div>
+                    </div>
+                </div>
+                <!-- Booking From -->
+                <div class="col-md-5 offset-md-2">
+                    <div class="booking-box">
+                        <div class="head-box">
+                            <h6>Escríbenos</h6>
+                            <h4>Estamos para atender tus dudas</h4>
+                        </div>
+                        <div class="booking-inner clearfix">
+                            <form action="https://duruthemes.com/demo/html/cappa/demo2/rooms2.html"
+                                class="form1 clearfix">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="input1_wrapper">
+                                            <label>Check in</label>
+                                            <div class="input1_inner">
+                                                <input type="text" class="form-control input datepicker"
+                                                    placeholder="Check in">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="input1_wrapper">
+                                            <label>Check out</label>
+                                            <div class="input1_inner">
+                                                <input type="text" class="form-control input datepicker"
+                                                    placeholder="Check out">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="select1_wrapper">
+                                            <label>Adults</label>
+                                            <div class="select1_inner">
+                                                <select class="select2 select" style="width: 100%">
+                                                    <option value="0">Adults</option>
+                                                    <option value="1">1</option>
+                                                    <option value="2">2</option>
+                                                    <option value="3">3</option>
+                                                    <option value="4">4</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="select1_wrapper">
+                                            <label>Children</label>
+                                            <div class="select1_inner">
+                                                <select class="select2 select" style="width: 100%">
+                                                    <option value="0">Children</option>
+                                                    <option value="1">1</option>
+                                                    <option value="2">2</option>
+                                                    <option value="3">3</option>
+                                                    <option value="4">4</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <button type="submit" class="btn-form1-submit mt-15">Check Availability</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Clients -->
+<section class="clients">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-7 owl-carousel owl-theme">
+                <div class="clients-logo">
+                    <a href="#0"><img src="/img/clients/1.png" alt=""></a>
+                </div>
+                <div class="clients-logo">
+                    <a href="#0"><img src="/img/clients/2.png" alt=""></a>
+                </div>
+                <div class="clients-logo">
+                    <a href="#0"><img src="/img/clients/3.png" alt=""></a>
+                </div>
+                <div class="clients-logo">
+                    <a href="#0"><img src="/img/clients/4.png" alt=""></a>
+                </div>
+                <div class="clients-logo">
+                    <a href="#0"><img src="/img/clients/5.png" alt=""></a>
+                </div>
+                <div class="clients-logo">
+                    <a href="#0"><img src="/img/clients/6.png" alt=""></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
