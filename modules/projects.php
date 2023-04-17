2ebd5681bbc279869185e324fd0edb3d417b5a92
Ivonne Mtz <Ivonnemtz96@outlook.es>
2023-04-17 14:40:27 -0700

    proyecto ok

---
+++ modules/projects.php
@@ -0,0 +1,131 @@
+<!-- Header Banner -->
+<div class="banner-header section-padding valign bg-img bg-fixed" data-overlay-dark="4"
+    data-background="/img/slider/13.webp">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 caption mt-90">
+                <span>
+                    <i class="star-rating"></i>
+                    <i class="star-rating"></i>
+                    <i class="star-rating"></i>
+                    <i class="star-rating"></i>
+                    <i class="star-rating"></i>
+                </span>
+                <h5>Estos son algunos de los proyectos más recientes desarrollados por: </h5>
+                <h1>XM Design</h1>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Rooms -->
+<section class="section-padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="rooms2 mb-90 animate-box" data-animate-effect="fadeInUp">
+                    <figure><img src="/img/slider/13.webp" alt="" class="img-fluid"></figure>
+                    <div class="caption">
+                        <h4><a href="/proyect-1">Dunas</a></h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates id officia non itaque ducimus adipisci!</p>
+                        <hr class="border-2">
+                        <div class="info-wrapper">
+                            <div class="butn-dark"> <a href="#" data-scroll-nav="1"><span>Ver proyecto</span></a> </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="rooms2 mb-90 left animate-box" data-animate-effect="fadeInUp">
+                    <figure><img src="/img/slider/14.webp" alt="" class="img-fluid"></figure>
+                    <div class="caption">
+                        <h4><a href="/proyect-1">Laguna Hill</a></h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates id officia non itaque ducimus adipisci!</p>
+                        <hr class="border-2">
+                        <div class="info-wrapper">
+                            <div class="butn-dark"> <a href="#" data-scroll-nav="1"><span>Ver proyecto</span></a> </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="rooms2 mb-90 animate-box" data-animate-effect="fadeInUp">
+                    <figure><img src="/img/slider/15.webp" alt="" class="img-fluid"></figure>
+                    <div class="caption">
+                        <h4><a href="/proyect-1">Palmilla</a></h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates id officia non itaque ducimus adipisci!</p>
+                        <hr class="border-2">
+                        <div class="info-wrapper">
+                            <div class="butn-dark"> <a href="#" data-scroll-nav="1"><span>Ver proyecto</span></a> </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="rooms2 left animate-box" data-animate-effect="fadeInUp">
+                    <figure><img src="/img/slider/16.webp" alt="" class="img-fluid"></figure>
+                    <div class="caption">
+                        <h4><a href="/proyect-1">Villa</a></h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates id officia non itaque ducimus adipisci!</p>
+                        <hr class="border-2">
+                        <div class="info-wrapper">
+                            <div class="butn-dark"> <a href="#" data-scroll-nav="1"><span>Ver proyecto</span></a> </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-12 mt-5">
+                <div class="rooms2 mb-90 animate-box" data-animate-effect="fadeInUp">
+                    <figure><img src="/img/slider/14.webp" alt="" class="img-fluid"></figure>
+                    <div class="caption">
+                        <h4><a href="/proyect-1">Villa</a></h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates id officia non itaque ducimus adipisci!</p>
+                        <hr class="border-2">
+                        <div class="info-wrapper">
+                            <div class="butn-dark"> <a href="#" data-scroll-nav="1"><span>Ver proyecto</span></a> </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="rooms2 mb-90 left animate-box" data-animate-effect="fadeInUp">
+                    <figure><img src="/img/slider/1.webp" alt="" class="img-fluid"></figure>
+                    <div class="caption">
+                        <h4><a href="/proyect-1">Olas 101</a></h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates id officia non itaque ducimus adipisci!</p>
+                        <hr class="border-2">
+                        <div class="info-wrapper">
+                            <div class="butn-dark"> <a href="#" data-scroll-nav="1"><span>Ver proyecto</span></a> </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="rooms2 mb-90 animate-box" data-animate-effect="fadeInUp">
+                    <figure><img src="/img/slider/5.webp" alt="" class="img-fluid"></figure>
+                    <div class="caption">
+                        <h4><a href="/proyect-1">Dunas 302</a></h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates id officia non itaque ducimus adipisci!</p>
+                        <hr class="border-2">
+                        <div class="info-wrapper">
+                            <div class="butn-dark"> <a href="#" data-scroll-nav="1"><span>Ver proyecto</span></a> </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="rooms2 left animate-box" data-animate-effect="fadeInUp">
+                    <figure><img src="/img/slider/17.webp" alt="" class="img-fluid"></figure>
+                    <div class="caption">
+                        <h4><a href="/proyect-1">Olas 102</a></h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates id officia non itaque ducimus adipisci!</p>
+                        <hr class="border-2">
+                        <div class="info-wrapper">
+                            <div class="butn-dark"> <a href="#" data-scroll-nav="1"><span>Ver proyecto</span></a> </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="rooms2 animate-box mt-5" data-animate-effect="fadeInUp">
+                    <figure><img src="/img/slider/18.webp" alt="" class="img-fluid"></figure>
+                    <div class="caption">
+                        <h4><a href="/proyect-1">Dunas 103</a></h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates id officia non itaque ducimus adipisci!</p>
+                        <hr class="border-2">
+                        <div class="info-wrapper">
+                            <div class="butn-dark"> <a href="#" data-scroll-nav="1"><span>Ver proyecto</span></a> </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
