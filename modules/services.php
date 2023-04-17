cfd2b1e7ec653e741b28d0278b2910faa70d3e9c
Ivonne Mtz <Ivonnemtz96@outlook.es>
2023-04-17 13:56:38 -0700

    servicios ok

---
+++ modules/services.php
@@ -0,0 +1,223 @@
+<!-- Header Banner -->
+<div class="banner-header section-padding valign bg-img bg-fixed" data-overlay-dark="4"
+    data-background="/img/slider/11.webp">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 caption mt-90">
+                <span>
+                    <i class="star-rating"></i>
+                    <i class="star-rating"></i>
+                    <i class="star-rating"></i>
+                    <i class="star-rating"></i>
+                    <i class="star-rating"></i>
+                </span>
+                <h5>Innova tu hogar con nuestro diseño de interiores personalizado</h5>
+                <h1>XM Desing</h1>
+            </div>
+        </div>
+    </div>
+</div>
+<section class="rooms1 section-padding bg-cream" data-scroll-index="1">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="item">
+                    <div class="position-re o-hidden"> <img src="/img/rooms/15.webp" alt=""> </div>
+                    <div class="con">
+                        <h5><a href="/contact">Jardinería</a> </h5>
+                        <div class="line"></div>
+                        <div class="row facilities">
+                            <div class="col col-md-5 text-right">
+                                <div class="permalink"><a href="/contact">Contactar <i
+                                            class="ti-arrow-right"></i></a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="item">
+                    <div class="position-re o-hidden"> <img src="/img/rooms/8.webp" alt=""> </div>
+                    <div class="con">
+                        <h5><a href="/contact">Consultor de diseño</a></h5>
+                        <div class="line"></div>
+                        <div class="row facilities">
+                            <div class="col col-md-5 text-right">
+                                <div class="permalink"><a href="/contact">Contactar <i
+                                            class="ti-arrow-right"></i></a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="item">
+                    <div class="position-re o-hidden"> <img src="/img/rooms/14.webp" alt=""> </div>
+                    <div class="con">
+                        <h5><a href="/contact">Diseño de jardines</a></h5>
+                        <div class="line"></div>
+                        <div class="row facilities">
+                            <div class="col col-md-5 text-right">
+                                <div class="permalink"><a href="/contact">Contactar <i
+                                            class="ti-arrow-right"></i></a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="item">
+                    <div class="position-re o-hidden"> <img src="/img/rooms/3.webp" alt=""> </div>
+                    <div class="con">
+                        <h5><a href="/contact">Diseño interior para residencias</a></h5>
+                        <div class="line"></div>
+                        <div class="row facilities">
+                            <div class="col col-md-5 text-right">
+                                <div class="permalink"><a href="/contact">Contactar <i
+                                            class="ti-arrow-right"></i></a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="item">
+                    <div class="position-re o-hidden"> <img src="/img/rooms/1.webp" alt=""> </div>
+                    <div class="con">
+                        <h5><a href="/contact">Remodelación de espacios</a></h5>
+                        <div class="line"></div>
+                        <div class="row facilities">
+                            <div class="col col-md-5 text-right">
+                            <div class="permalink"><a href="/contact">Contactar <i
+                                            class="ti-arrow-right"></i></a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="item">
+                    <div class="position-re o-hidden"> <img src="/img/rooms/16.webp" alt=""> </div>
+                    <div class="con">
+                        <h5><a href="/contact">Tapicería y Cortinas</a> </h5>
+                        <div class="line"></div>
+                        <div class="row facilities">
+                            <div class="col col-md-5 text-right">
+                                <div class="permalink"><a href="/contact">Contactar <i
+                                            class="ti-arrow-right"></i></a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="item">
+                    <div class="position-re o-hidden"> <img src="/img/rooms/17.webp" alt=""> </div>
+                    <div class="con">
+                        <h5><a href="/contact">Limpieza de cortinas</a></h5>
+                        <div class="line"></div>
+                        <div class="row facilities">
+                            <div class="col col-md-5 text-right">
+                                <div class="permalink"><a href="/contact">Contactar <i
+                                            class="ti-arrow-right"></i></a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="item">
+                    <div class="position-re o-hidden"> <img src="/img/rooms/18.webp" alt=""> </div>
+                    <div class="con">
+                        <h5><a href="/contact">Limpieza de alfombras</a></h5>
+                        <div class="line"></div>
+                        <div class="row facilities">
+                            <div class="col col-md-5 text-right">
+                                <div class="permalink"><a href="/contact">Contactar <i
+                                            class="ti-arrow-right"></i></a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="item">
+                    <div class="position-re o-hidden"> <img src="/img/rooms/19.webp" alt=""> </div>
+                    <div class="con">
+                        <h5><a href="/contact">Limpieza de muebles</a></h5>
+                        <div class="line"></div>
+                        <div class="row facilities">
+                            <div class="col col-md-5 text-right">
+                                <div class="permalink"><a href="/contact">Contactar <i
+                                            class="ti-arrow-right"></i></a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="item">
+                    <div class="position-re o-hidden"> <img src="/img/rooms/20.webp" alt=""> </div>
+                    <div class="con">
+                        <h5><a href="/contact">Limpieza de tapicería</a></h5>
+                        <div class="line"></div>
+                        <div class="row facilities">
+                            <div class="col col-md-5 text-right">
+                                <div class="permalink"><a href="/contact">Contactar <i
+                                            class="ti-arrow-right"></i></a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="item">
+                    <div class="position-re o-hidden"> <img src="/img/rooms/10.webp" alt=""> </div>
+                    <div class="con">
+                        <h5><a href="/contact">Diseño interior para propiedades comerciales</a> </h5>
+                        <div class="line"></div>
+                        <div class="row facilities">
+                            <div class="col col-md-5 text-right">
+                                <div class="permalink"><a href="/contact">Contactar <i
+                                            class="ti-arrow-right"></i></a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="item">
+                    <div class="position-re o-hidden"> <img src="/img/rooms/6.webp" alt=""> </div>
+                    <div class="con">
+                        <h5><a href="/contact">Decoración de interiores para residencias</a> </h5>
+                        <div class="line"></div>
+                        <div class="row facilities">
+                            <div class="col col-md-5 text-right">
+                                <div class="permalink"><a href="/contact">Contactar <i
+                                            class="ti-arrow-right"></i></a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="item">
+                    <div class="position-re o-hidden"> <img src="/img/rooms/7.webp" alt=""> </div>
+                    <div class="con">
+                        <h5><a href="/contact">Decoración de interiores para locales comerciales</a> </h5>
+                        <div class="line"></div>
+                        <div class="row facilities">
+                            <div class="col col-md-5 text-right">
+                                <div class="permalink"><a href="/contact">Contactar <i
+                                            class="ti-arrow-right"></i></a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
